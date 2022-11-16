bc7fcf8e5d3ef9ce457b30341737068cb1801760
Carllos166 <carlos_rj18@hotmail.com>
2022-11-15 22:44:56 -0300

    promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("foi");
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then(function (response) { // aguardando uma promise = assíncrono 
+        return response.json()
+    })
+    .then(function(jsonBody) {
+        console.log(jsonBody)
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
+    .finally(function () {
+        console.log('Requisição conluída!')
+    })
