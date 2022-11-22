9b8e8fcc7773304dbb1d6f7ffc3f49fa0bb63a81
Carllos166 <carlos_rj18@hotmail.com>
2022-11-22 00:37:31 -0300

    API pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,37 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                    <ol class="types">
+                <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+// aguardando uma promise = assíncrono
 fetch(url)
-    .then(function (response) { // aguardando uma promise = assíncrono 
-        return response.json()
+    .then((response) => response.json()) // then promise for resolvida => convertando body para json
+    .then((jsonBody) => jsonBody.results) // coletando .result que é a lista de pokemon
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }
     })
-    .then(function(jsonBody) {
-        console.log(jsonBody)
-    })
-    .catch(function (error) {
-        console.error(error)
-    })
-    .finally(function () {
-        console.log('Requisição conluída!')
-    })
+    .catch((error) => console.error(error))
