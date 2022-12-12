d7ee96d6b1e818d8026e5ac1a437b2e2732bd45f
Carllos166 <carlos_rj18@hotmail.com>
2022-12-11 23:50:04 -0300

    Criando minha propia lista de pokemon escolhendo o que irá vir da requisição

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,24 +1,37 @@
- 
+
 
 
 const pokeApi = {}
 
-pokeApi.getPokemons = async (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon() // Start new Instance / Object
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
 
-    try {
-        const response = await fetch(url)
-        const jsonBody = await response.json()
-        return jsonBody.results
-    } catch (error) {
-        return console.error(error)
-    }
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [typeMain] = types
+
+    pokemon.types = types
+    pokemon.type = typeMain
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default    
+    return pokemon
 }
 
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4')
-]).then((results) => console.log(results))
- 
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+
+}
